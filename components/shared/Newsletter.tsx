24700ff82450656bc5ccb83a5d2784601bb1b460
maludatech <nwitevictor7@gmail.com>
2025-08-08 23:03:31 +0100

    update: done with newsletter component

--- components/shared/Newsletter.tsx
+++ components/shared/Newsletter.tsx
@@ -1,7 +1,28 @@
 "use client";
 
 import React from "react";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
 
 export const NewsLetter: React.FC = () => {
-  return <section className="w-full py-20 bg-muted"></section>;
+  return (
+    <section className="w-full pt-16">
+      <div className="container mx-auto flex flex-col justify-center items-center w-full gap-6 px-6 md:px-12">
+        <div className="flex flex-col gap-2 items-center justify-center">
+          <h1 className="text-3xl font-bold text-[#0B1909]">
+            Subscribe to our newsletter for the latest updates
+          </h1>
+          <p className="text-black max-w-xl text-center">
+            We’re putting the final touches on something powerful. Join our
+            early user list to get notified when MedIntel officially launches,
+            no spam, just updates.
+          </p>
+        </div>
+        <div className="flex gap-2 items-center justify-center w-full max-w-md">
+          <Input placeholder="Email Address" className="w-full" />
+          <Button className="text-black cursor-pointer">Notify Me</Button>
+        </div>
+      </div>
+    </section>
+  );
 };
