0405810fd9ac41a78f24232399e62d7cf2bfe49b
maludatech <nwitevictor7@gmail.com>
2025-08-08 23:22:45 +0100

    update: footer started

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,17 +1,28 @@
 "use client";
+import images from "@/public/assets/images";
 
 export const Footer = () => {
-  const links = [
-    { name: "Home", href: "/" },
-    { name: "Predict", href: "/predict" },
-    { name: "About", href: "/about" },
-    { name: "FAQ", href: "/faq" },
-    { name: "Contact", href: "/contact" },
-  ];
-
   return (
     <footer className="bg-[#E6F7E4] border-t border-border pt-16">
-      <div className="container mx-auto flex flex-col w-full gap-10 px-6 md:px-12"></div>
+      <div
+        className="container mx-auto flex flex-col w-full gap-10 px-6 md:px-12 pt-16"
+        style={{
+          backgroundImage: `url(${images.footerBackgroundImage.src})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <div className="flex flex-col gap-4 items-center justify-center">
+          <h2 className="text-4xl font-semibold text-center max-w-2xl">
+            Predict Health Issues Smarter with MedIntel
+          </h2>
+          <p className="text-sm font-semibold max-w-md">
+            Leverage AI to identify potential diseases early, track your health
+            over time, and make informed decisions all from one intelligent
+            platform.
+          </p>
+        </div>
+      </div>
     </footer>
   );
 };
--- public/assets/images/index.ts
+++ public/assets/images/index.ts
@@ -1,7 +1,15 @@
 import hero from "./hero-image.png";
 import icon from "./icon.png";
+import footerBackgroundImage from "./footer-bg-img.png";
 import howItWorks1 from "./how-it-works/how-it-works-1.png";
 import howItWorks2 from "./how-it-works/how-it-works-2.png";
 import howItWorks3 from "./how-it-works/how-it-works-3.png";
 
-export default { hero, icon, howItWorks1, howItWorks2, howItWorks3 };
+export default {
+  hero,
+  icon,
+  footerBackgroundImage,
+  howItWorks1,
+  howItWorks2,
+  howItWorks3,
+};
